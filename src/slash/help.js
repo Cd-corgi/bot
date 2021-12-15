25dcc323795663e0e832e596643f5997a623183e
corgi <creep3rcor7@gmail.com>
2021-12-15 09:25:04 -0500

    bunny

--- src/slash/help.js
+++ src/slash/help.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const { MessageEmbed } = require('discord.js');
+const { MessageEmbed, MessageButton } = require('discord.js');
 const Discord = require('discord.js');
 
 module.exports = {
@@ -7,6 +7,13 @@ module.exports = {
     .setName("help")
     .setDescription("It show you some advices about the commands"),
     async run(client, interaction){
-        
+        const General = new MessageEmbed()
+        .setTitle(`${interaction.guild.me.unsername}\'s Help Menu!`)
+        .setDescription(`The majority of the commands are built in \`/\` Commands!\n**They has several ways to use them!**\n> **Alterative functions!**\n>\`/\`warnings <add/check/remove>`)
+        .setColor("YELLOW")
+
+        interaction.reply({
+            embeds: [General]
+        })
     }
 }
