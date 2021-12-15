a35af1986c397921bd6ac32a9b420fa687ab70d2
corgi <creep3rcor7@gmail.com>
2021-12-15 13:00:21 -0500

    bunny

--- src/slash/help.js
+++ src/slash/help.js
@@ -1,19 +1,51 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { MessageEmbed, MessageButton } = require('discord.js');
 const Discord = require('discord.js');
+const paginationembed = require('discordjs-button-pagination')
+const fs = require('fs')
 
 module.exports = {
     data: new SlashCommandBuilder()
-    .setName("help")
-    .setDescription("It show you some advices about the commands"),
-    async run(client, interaction){
+        .setName("help")
+        .setDescription("It show you some advices about the commands"),
+    async run(client, interaction) {
         const General = new MessageEmbed()
-        .setTitle(`${client.bot.username}\'s Help Menu!`)
-        .setDescription(`The majority of the commands are built in \`/\` Commands!\n**They has several ways to use them!**\n> **Alterative functions!**\n>\`/\`warnings <add/check/remove>`)
-        .setColor("YELLOW")
+            .setTitle(`Bunny Hops\' Help Menu!`)
+            .setDescription(`The majority of the commands are built in \`/\` Commands!\n**They has several ways to use them!**\n > **Alterative functions!**\n>\`/\`warnings <add/check/remove>`)
+            .setColor("YELLOW")
 
-        interaction.reply({
-            embeds: [General]
-        })
+        const cms = fs.readdir('../slash/').filter(f => f.endsWith('.js'))
+
+        for (const f of cms) {
+
+            let cmdS = require(`../slash/${f}`);
+
+            const Mod = new MessageEmbed()
+                .setTitle(`Bunny Hops\' Help Menu!`)
+                .setDescription(client.slashCommands.map(z => `\`/${z.data.name}\``).join("\n"))
+                .setColor("PINK")
+        }
+
+        const button1 = new MessageButton()
+                .setCustomId('previousbtn')
+                .setLabel('Previous')
+                .setStyle('DANGER');
+
+        const button2 = new MessageButton()
+                .setCustomId('nextbtn')
+                .setLabel('Next')
+                .setStyle('SUCCESS');
+
+        let pages = [
+            General,
+            Mod
+        ];
+
+        let buttonList = [
+            button1,
+            button2
+        ]
+
+        paginationembed(interaction, pages, buttonList, tiemout)
     }
 }
