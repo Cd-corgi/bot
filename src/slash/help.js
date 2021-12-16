be2c9c3c79e65bd0f1bd5349b29daa5dcb14b7aa
corgi <creep3rcor7@gmail.com>
2021-12-16 09:40:35 -0500

    bunny

---
+++ src/slash/help.js
@@ -0,0 +1,24 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed } = require('discord.js');
+const Discord = require('discord.js');
+const fs = require('fs')
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName("help")
+    .setDescription("it shows yuosome advices about the commands"),
+    async run(client, interaction){
+        var cmds = [];
+
+        let scmd = fs.readdirSync('../slash/').filter(file => file.endsWith('.js'));
+
+        for(const file of scmd) {
+            
+            let com = fs.readdirSync(`../slash/${file}`);
+            cmds.push(com.toString());
+            console.log(cmds.join("\n"))
+        }
+
+        
+    }
+}
