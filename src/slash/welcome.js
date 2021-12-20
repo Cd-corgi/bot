3c2aed9dfefa67c5948e3dc1ed3b560421ec81e4
corgi <creep3rcor7@gmail.com>
2021-12-20 09:28:59 -0500

    bunny

--- src/slash/welcome.js
+++ src/slash/welcome.js
@@ -39,7 +39,7 @@ module.exports = {
         const txt = interaction.options.getString("message") || `{user}! Welcome to ${interaction.guild.name}`;
 
         if(choice === "set") {
-            wel.findOne({ guildID: interaction.guild.id, channelID: chan.id }, (err, data) => {
+            wel.findOne({ guildID: interaction.guild.id, channelID: chan.id }, async (err, data) => {
                 if(err) throw err;
                 if(!data) {
                     data = new wel({
