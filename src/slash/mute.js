d40979943970a5f0ab8d4bf2527daa552c0c240b
corgi <creep3rcor7@gmail.com>
2021-12-24 14:35:47 -0500

    bunny

---
+++ src/slash/mute.js
@@ -0,0 +1,43 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed } = require('discord.js');
+const Discord = require('discord.js');
+const ms = require('ms');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName("mute")
+    .setDescription("Mutes a user with time!")
+    .addUserOption(option =>
+        option
+        .setName("user")
+        .setDescription("Mention someone to mute!")
+        .setRequired(true)
+    )
+    .addStringOption(option =>
+        option
+        .setName("length")
+        .setDescription("Provide how long will last the mute!")
+        .setRequired(true)
+    )
+    .addStringOption(option =>
+        option
+        .setName("reason")
+        .setDescription("Provice the reason of the mute!")
+        .setRequired(false)
+    ),
+    async run(client, interaction){
+        const user = interaction.options.getUser("user");
+        const duration = interaction.options.getString("length");
+        const Reason = interaction.options.getString("reason") || "Without Reason";
+
+        const member = interaction.guild.members.cache.get(user.id);
+
+        const timeoutMs = ms(duration);
+
+        if(!timeoutMs) return interaction.reply("Specify a valid time!")
+
+        member.timeout(timeoutMs, reason)
+
+        interaction.reply(`${member.username} have been muted!`)
+    }
+}
