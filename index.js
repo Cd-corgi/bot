83f156395ec54a97da68cbc15ac8bfc6a701c024
corgi <creep3rcor7@gmail.com>
2021-12-27 11:58:12 -0500

    bunny

--- index.js
+++ index.js
@@ -113,8 +113,8 @@ client.on("guildMemberAdd", async (member, guild) => {
 
 client.on("messageCreate", async (message) => {
 
-    let asp = "";
-    let wel = "";
+    let asp = " ";
+    let wel = " ";
 
     //#region check anti-spam
         Nospam.findOne({ guildID: message.guild.id }, async(err, data) => {
