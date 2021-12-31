a5b509c789ba1e9436a72e09704c1fe98cfddc17
corgi <creep3rcor7@gmail.com>
2021-12-31 13:55:32 -0500

    bunny

--- index.js
+++ index.js
@@ -236,9 +236,13 @@ const alone = new Discord.MessageEmbed()
 
 client.distube.on("finish", queue => queue.textChannel.send({ embeds: [fisi] }))
 client.distube.on("empty", queue => queue.textChannel.send({ embeds: [alone] }))
-client.distube.on("searchResult", (message, result) => {
-    let i = 0;
-    message.channel.send(`**Please select one of the listed results!** \n${result.map(song => `**${++i}** - \`${song.name}\` - **${song.formattedDuration}**`).join("\n")}\n**Type anything or wait 30 seconds to cancel**`)
+client.distube.on("searchResult", (message, result, song, args) => {
+    let i = 0
+    message.channel.send('Searching...').then(msg => {
+    msg.edit(`**Choose an option from below**\n${result.map(song => `**${++i}**. ${song.name} - \`${song.formattedDuration}\``).join("\n")}\n*Enter anything else or wait 60 seconds to cancel*`).then(msg => {
+        setTimeout(() => msg.delete(), 15000)
+      })
+})
 })
 client.distube.on("searchCancel", message => { message.channel.send("Search Canceled!") })
 client.distube.on("searchInvalidAnswer", message => { message.channel.send("Invalid numer of the song!")})
