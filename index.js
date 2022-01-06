50de39a0d968855a81e4f3b9aafbc5810a04d447
corgi <creep3rcor7@gmail.com>
2022-01-06 17:06:02 -0500

    bunny

--- index.js
+++ index.js
@@ -257,7 +257,7 @@ client.on("messageCreate", async (message) => {
 
 //#region event detection
 client.on("guildMemberUpdate", async (oldMember, newMember) => {
-    let bg = await boost.findOne({ guild: newMember.guild.id })
+    let bg = boost.findOne({ guild: newMember.guild.id })
 
     if (!bg) {
         return;
