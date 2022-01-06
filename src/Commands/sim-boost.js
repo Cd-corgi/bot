e9c290728aeaab1dd82d01c33efd2eeda14c88f8
corgi <creep3rcor7@gmail.com>
2022-01-06 16:17:01 -0500

    bunny

--- src/Commands/sim-boost.js
+++ src/Commands/sim-boost.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'sim',
     alias: ["boost"],
     run: async(client, message, args) => {
-        client.emit("guildMemberUptdate", message.member)
+        client.emit("guildMemberUpdate", message.member)
     }
 }
