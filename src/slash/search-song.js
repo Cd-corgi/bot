12e62b9399526d428eb4e1f5c5fe93b191c86f2e
corgi <creep3rcor7@gmail.com>
2022-01-07 11:54:55 -0500

    bunny

--- src/slash/search-song.js
+++ src/slash/search-song.js
@@ -46,7 +46,7 @@ module.exports = {
         })
 
         let userinput;
-        const resultC = await msg.awaitMessages({
+        const resultC = await msg.awaitInteractions({
             max: 1,
             time: 60000,
             errors: ["time"],
