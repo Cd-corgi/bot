59b230bf146fd79885ac209dd536bd7de92000c0
corgi <creep3rcor7@gmail.com>
2022-01-07 08:39:57 -0500

    bunny

--- src/Commands/sim-boost.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'sim',
-    alias: ["boost"],
-    run: async(client, message, args) => {
-        client.emit("guildMemberUpdate", message.member)
-    }
-}
--- src/models/boost.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-let boosted = new mongoose.Schema({
-    guild: String,
-    channel: String
-});
-
-module.exports = mongoose.model('boost', boosted, 'Boost Notify');
