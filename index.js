fd9538c99e7ac7b042aadaba5176efda6e6cddfe
corgi <creep3rcor7@gmail.com>
2022-01-08 09:44:03 -0500

    bunny

--- index.js
+++ index.js
@@ -265,7 +265,7 @@ const SpotifyPlugin = require('@distube/spotify');
 client.distube = new Distube.default(client, {
     leaveOnFinish: true,
     leaveOnEmpty: true,
-    searchSongs: 10,
+    searchSongs: 2,
     searchCooldown: 5,
     emptyCooldown: 0,
     // plugins: [new SoundCloudPlugin(), new SpotifyPlugin()]
