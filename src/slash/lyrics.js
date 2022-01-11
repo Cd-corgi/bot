8dd8de9b341951e8e8e3a4671c8d4434a6282faa
corgi <creep3rcor7@gmail.com>
2022-01-11 18:32:14 -0500

    bunny

--- src/slash/lyrics.js
+++ src/slash/lyrics.js
@@ -8,18 +8,12 @@ const Client = new Genius.Client(GENIUS_API)
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("lyrics")
-        .setDescription("Let the bot find your lyrics!")
-        .addStringOption(option =>
-            option
-                .setName("song-name")
-                .setDescription("Provide the song to search, or search the current song!")
-                .setRequired(false)
-        ),
+        .setDescription("Let the bot find your current song's Lyrics"),
     async run(client, interaction) {
         const query = interaction.options.getString("song-name");
         const queue = client.distube.getQueue(interaction.member.voice.channel);
 
-        if (queue && !query) { //only emiting the current song in vc
+        if (queue) { //only emiting the current song in vc
             let song = queue.songs[0];
 
             const searches = await Client.songs.search(queue.songs[0].name);
@@ -46,35 +40,12 @@ module.exports = {
                     ]
                 })
             }, 5000)
-        } else
-            if (query && !queue) { // only emiting in case if song-name contains data!
-                let src2 = await Client.songs.search(query);
-
-                let fsSongs = src2[0];
-
-                let lyr = await fsSongs.lyrics();
-                let nsm = await fsSongs.title();
-                let tape = await fsSongs.thumbnail();
-
-                interaction.reply({
-                    embeds: [
-                        new MessageEmbed()
-                            .setTitle(`Looking for ${nsm} ...`)
-                            .setColor("YELLOW")
-                    ]
-                })
-
-                setTimeout(() => {
-                    interaction.editReply({
-                        embeds: [
-                            new MessageEmbed()
-                                .setTitle(`${nsm}`)
-                                .setDescription(`${(lyr).substring(0, 4096)}`)
-                                .setColor("LUMINOUS_VIVID_PINK")
-                        ]
-                    })
-                }, 5000)
-            }
+        } else { 
+            interaction.reply({
+                content: 'Theres no song to fetch their lyrics!'
+            }).then(() => setTimeout(() => interaction.deleteReply(), 5000))
+            return;
+        }
 
     }
 }
