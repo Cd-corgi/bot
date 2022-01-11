e7299fc374da6289c9266417825217a2da7be57d
corgi <creep3rcor7@gmail.com>
2022-01-11 18:27:10 -0500

    bunny

--- src/slash/lyrics.js
+++ src/slash/lyrics.js
@@ -19,7 +19,7 @@ module.exports = {
         const query = interaction.options.getString("song-name");
         const queue = client.distube.getQueue(interaction.member.voice.channel);
 
-        if (queue) {
+        if (queue && !query) { //only emiting the current song in vc
             let song = queue.songs[0];
 
             const searches = await Client.songs.search(queue.songs[0].name);
@@ -39,16 +39,42 @@ module.exports = {
                 interaction.editReply({
                     embeds: [
                         new MessageEmbed()
-                            .setAuthor(`📨 | Lyrics`, song.thumbnail)
+                            .setTitle(`${song.name}`)
                             .setThumbnail(`${song.thumbnail}`)
                             .setDescription(`${(lyrics).substring(0, 4096)}`)
                             .setColor("LUMINOUS_VIVID_PINK")
                     ]
                 })
             }, 5000)
-        } else {
-            return;
-        }
+        } else
+            if (query && !queue) { // only emiting in case if song-name contains data!
+                let src2 = await Client.songs.search(query);
+
+                let fsSongs = src2[0];
+
+                let lyr = await fsSongs.lyrics();
+                let nsm = await fsSongs.title();
+                let tape = await fsSongs.thumbnail();
+
+                interaction.reply({
+                    embeds: [
+                        new MessageEmbed()
+                            .setTitle(`Looking for ${nsm} ...`)
+                            .setColor("YELLOW")
+                    ]
+                })
+
+                setTimeout(() => {
+                    interaction.editReply({
+                        embeds: [
+                            new MessageEmbed()
+                                .setTitle(`${nsm}`)
+                                .setDescription(`${(lyr).substring(0, 4096)}`)
+                                .setColor("LUMINOUS_VIVID_PINK")
+                        ]
+                    })
+                }, 5000)
+            }
 
     }
 }
